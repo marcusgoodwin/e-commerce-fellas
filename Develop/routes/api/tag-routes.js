ac545d345ac2ea5f097dec50277d5c1793304f0d
unknown <mwgoodwin2000@gmail.com>
2023-03-09 16:03:30 -0700

    editting

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,26 +3,75 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const tags = await Tag.findAll({
+      include: Product
+    });
+    res.send(tags);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+
+router.get('/:id', async (req, res) => {
+  try {
+    const tag = await Tag.findByPk(req.params.id, {
+      include: Product
+    });
+    if (!tag) {
+      return res.status(404).send('Tag not found');
+    }
+    res.send(tag);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+
+router.post('/', async (req, res) => {
+  try {
+    const tag = await Tag.create(req.body);
+    res.send(tag);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+
+router.put('/:id', async (req, res) => {
+  try {
+    const tag = await Tag.findByPk(req.params.id);
+    if (!tag) {
+      return res.status(404).send('Tag not found');
+    }
+    tag.name = req.body.name;
+    await tag.save();
+    res.send(tag);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const tag = await Tag.findByPk(req.params.id);
+    if (!tag) {
+      return res.status(404).send('Tag not found');
+    }
+    await tag.destroy();
+    res.send('Tag deleted');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
 });
 
-module.exports = router;
+
+module.exports = routes;
